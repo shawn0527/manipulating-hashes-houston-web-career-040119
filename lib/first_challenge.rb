3681b0db4013123d287bdb44fef6967193be2f7d
Shawn Ren <doggy-ren@hotmail.com>
2019-03-12 02:20:44 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,7 +12,7 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-  contacts["Freddy Mercury"][favorite_icecream_flavors:].delete_if {|flacvor| flavor = "strawberry"}
+  contacts["Freddy Mercury"][favorite_icecream_flavors:].delete_if {|flacvor| flavor == "strawberry"}
   #your code here
 
 
